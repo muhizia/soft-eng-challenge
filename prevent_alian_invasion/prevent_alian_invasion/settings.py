ac027ade371dcb6e1695d861405148f69508dbb3
Aristide Muhizi <muhizia4@gmail.com>
2022-05-10 08:56:07 +0200

    removing the secret key

--- prevent_alian_invasion/prevent_alian_invasion/settings.py
+++ prevent_alian_invasion/prevent_alian_invasion/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wt&9==*c&!%ee7=qjm++#=*3k@!9e^f1-p=5^=q1%15p7&x+^('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
